fd12fd01336b004dc9cf42cca7c001d2b63cc225
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-06-26 15:50:49 +0200

    make captions working

--- collective/videoanysurfer/browser/link_view.py
+++ collective/videoanysurfer/browser/link_view.py
@@ -1,11 +1,14 @@
-from zope.component import getMultiAdapter
+from zope import component
 from zope import interface
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from collective.configviews.browser.configurable_view import ConfigurableBaseView
-from collective.videoanysurfer.video import IVideoAnySurfer
+from collective.videoanysurfer.video import IVideoAnySurfer, IVideoExtraData
 from urlparse import urlparse
 from urllib import urlopen
 from Products.Five.browser import BrowserView
+from plone.autoform.form import AutoExtensibleForm
+from z3c.form import form
+from AccessControl.security import checkPermission
 YOUTUBE_TRANS = "http://video.google.com/timedtext?lang=%(lang)s&v=%(vid)s"
 
 
@@ -19,6 +22,7 @@ class LinkView(ConfigurableBaseView):
         self.youtube = None
         self.vid = None
         self.portal_state = None
+        self.extra = None
 
     def update(self):
         if self.youtube is None:
@@ -33,9 +37,12 @@ class LinkView(ConfigurableBaseView):
             self.youtube = domain and scheme and path and vid
 
         if self.portal_state is None:
-            self.portal_state = getMultiAdapter((self.context,
-                                                 self.request),
+            self.portal_state = component.getMultiAdapter((self.context,
+                                                          self.request),
                                                 name=u'plone_portal_state')
+        if self.extra is None:
+            self.extra = component.queryAdapter(self.context, IVideoExtraData)
+            self.extra.update()
 
     def __call__(self):
         self.update()
@@ -57,9 +64,11 @@ class LinkView(ConfigurableBaseView):
         return self.context.Title()
 
     def captions(self):
-        nav_url = self.portal_state.navigation_root_url()
-        if self.is_youtube():
-            return "%s/@@videoanysurfer_transcription" % nav_url
+        captions = self.extra.captions
+        if not captions:
+            return
+
+        return "%s/@@videoanysurfer_captions" % self.context.absolute_url()
 
     def is_youtube(self):
         return self.youtube
@@ -67,12 +76,43 @@ class LinkView(ConfigurableBaseView):
     def language(self):
         return self.portal_state.language()
 
+    def canedit(self):
+        return checkPermission("cmf.ModifyPortalContent", self.context)
 
-class VideoTranscription(BrowserView):
+    def context_url(self):
+        return self.context.absolute_url()
+
+
+class VideoCaptions(BrowserView):
 
     def __call__(self):
-        vid = self.request.get('vid', None)
-        lang = self.request.get('lang', 'en')
-        query = {'lang': lang, 'vid': vid}
-        import pdb;pdb.set_trace()
-        return urlopen(YOUTUBE_TRANS % query).read()
+        self.request.response.setHeader('X-Theme-Disabled', 'True')
+        self.request.response.setHeader('Content-Type', 'text/xml')
+        extra = component.queryAdapter(self.context, IVideoExtraData)
+        if extra:
+            extra.update()
+            return extra.captions
+
+
+class VideoExtraDataEditForm(AutoExtensibleForm, form.EditForm):
+    schema = IVideoExtraData
+
+    def getContent(self):
+        extra = component.queryAdapter(self.context, IVideoExtraData)
+        if extra:
+            extra.update()
+            return {'captions': extra.captions,
+                    'transcription': extra.transcription}
+
+    def applyChanges(self, data):
+        extra = component.queryAdapter(self.context, IVideoExtraData)
+        if extra:
+            extra.update()
+            extra.captions = data.get('captions', '')
+            extra.transcription = data.get('transcription', '')
+            self.request.response.redirect(self.context.absolute_url())
+            return True
+
+from plone.app.z3cform.layout import wrap_form
+
+VideoExtraDataEditFormView = wrap_form(VideoExtraDataEditForm)
--- collective/videoanysurfer/video.py
+++ collective/videoanysurfer/video.py
@@ -1,6 +1,8 @@
 from zope import interface
 from zope import schema
 from zope import i18nmessageid
+from zope.annotation.interfaces import IAnnotations
+import persistent
 _ = i18nmessageid.MessageFactory('collective.videoanysurfer')
 
 
@@ -10,19 +12,87 @@ subtitles in it to let people being able to watch the video. """)
 
 transcription_title = _(u"Transcription")
 transcription_desc = _(u"""You may provide the audio transcription of the
-current video here.""")
+current video here. It is pure text""")
 transcription_uri_title = _(u"Transcription URL")
 transcription_uri_desc = _(u"Please push the XML transcription file to use")
+transcription_raw_title = _(u"Transcription Raw text")
+transcription_raw_desc = _(u"This text will be displayed next to the video")
 
 
 class IVideoAnySurfer(interface.Interface):
     """Any surfer 2.0 metadata"""
 
     download_uri = schema.URI(title=download_url_title,
-                              description=download_url_desc)
+                              description=download_url_desc,
+                              required=False)
+#
+#    transcription_uri = schema.URI(title=transcription_uri_title,
+#                                   description=transcription_uri_desc,
+#                                   required=False)
+#
+#    transcription_xml = schema.Text(title=transcription_title,
+#                                description=transcription_desc,
+#                                required=False)
+#
+#    transcription_raw = schema.Text(title=transcription_raw_title,
+#                                    description=transcription_raw_desc,
+#                                    required=False)
 
-    transcription_uri = schema.URI(title=transcription_uri_title,
-                                   description=transcription_uri_desc)
+captions_title = _(u"Captions")
+captions_desc = _(u"XML content to display during video playing")
+
+
+class IVideoExtraData(interface.Interface):
+    """Adding data to improve video accessibility"""
+
+    captions = schema.Text(title=captions_title,
+                           description=captions_desc)
 
     transcription = schema.Text(title=transcription_title,
-                                description=transcription_desc)
+                                description=transcription_desc,
+                                required=False)
+
+VIDEO_EXTRADATA_KEY = "collective.videoanysurfer.videoextradata"
+
+
+class VideoExtraData(object):
+    """implement video extra data using annotation"""
+    interface.implements(IVideoExtraData)
+
+    def __init__(self, context):
+        self.context = context
+        self.storage = None
+        self._annotations = None
+
+    def update(self):
+        if self.storage is None:
+            self._annotations = IAnnotations(self.context)
+            if self._annotations.get(VIDEO_EXTRADATA_KEY,
+                                     None) is None:
+                self.storage = persistent.dict.PersistentDict()
+                self._annotations[VIDEO_EXTRADATA_KEY] = self.storage
+            else:
+                self.storage = self._annotations[VIDEO_EXTRADATA_KEY]
+
+    def get_captions(self):
+        return self.storage.get('captions', '')
+
+    def set_captions(self, value):
+        self.storage['captions'] = value
+
+    def del_captions(self):
+        del self.storage['captions']
+
+    captions = property(get_captions, set_captions, del_captions)
+
+    def get_transcription(self):
+        return self.storage.get('transcription', '')
+
+    def set_transcription(self, value):
+        self.storage['transcription'] = value
+
+    def del_transcription(self):
+        del self.storage['transcription']
+
+    transcription = property(get_transcription, set_transcription,
+                             del_transcription)
